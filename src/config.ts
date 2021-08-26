25daded20a4460fc7bdb1bf1db0146b977810c0b
Michał Korman <michal.korman@icloud.com>
2021-08-27 01:48:09 +0200

    fix docker-compose runtime

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 export const TOKEN = process.env.TOKEN;
 
-export const DB_DIR_LOCATION = process.env.DB_DIR_LOCATION || './db';
+export const DB_DIR_LOCATION = process.env.DB_DIR_LOCATION || './.db';
 
 export const TIMEZONE = process.env.TIMEZONE || 'UTC';
 export const LANGUAGE = process.env.LANGUAGE || 'pl';
