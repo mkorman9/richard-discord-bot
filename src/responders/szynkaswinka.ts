37617dce1976b4ce6057506041abaf760354a685
Michał Korman <michal.korman@icloud.com>
2021-09-04 17:08:05 +0200

    fix regex in szynkaswinka responder

--- src/responders/szynkaswinka.ts
+++ src/responders/szynkaswinka.ts
@@ -1,7 +1,7 @@
 import type { ResponderExecutionProps, ResponderManifest } from './module';
 
 const callback = (props: ResponderExecutionProps): boolean => {
-  if (props.message.content.search(/\bszynka\b/i) >= 0) {
+  if (props.message.content.search(/(\b|[^\w])szynka(\b|[^\w])/i) >= 0) {
     props.message.reply('> [...] szynka [...] \n\n świnka :D \n\n https://i.kym-cdn.com/entries/icons/original/000/008/001/Spurdo.jpg');
     return true;
   }
