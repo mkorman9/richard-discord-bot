eba9b83e8641b9d6b182c13f03a69bf9cde0ad57
Michał Korman <michal.korman@icloud.com>
2021-12-21 17:53:00 +0100

    lint for support scripts

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 mkdir -p "${SCRIPT_DIR}/.db" && \
   chmod u+rwx "${SCRIPT_DIR}/.db"
 
-export DOCKER_UID=$(id -u)
-export DOCKER_GID=$(id -g)
+export USER_UID=$(id -u)
+export USER_GID=$(id -g)
 
 docker-compose up -d --build bot
